fe641a7586a65b63da0673b40adbac722454e1cf
collins <collinsboniface1000@gmail.com>
2022-05-03 01:49:18 +0300

    Explored more about the Variables and Printf or Print format to formart our print statement using the print formart

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,5 +7,23 @@ import "fmt"
 // The "main" function is the entrypoint of a Go Program
 
 func main() {
-	fmt.Println("Hello World")
+	/*
+	 Variables are used to store values
+	 Like containers for values 
+	 Give the variable a name and reference everywhere in the app
+	 Variable names should be descriptive
+	*/
+	
+
+	var conferenceName = "Go Conference"
+
+	// Constants are like variables, except that their values cannot be changed
+	const conferenceTickets = 50
+	var remainingTickets = 50	
+
+	fmt.Printf("Welcome to %v booking application. \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+
+
 }
