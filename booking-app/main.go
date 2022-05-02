b2e08e2409e4aadc72198e65211fb6cf50403055
collins <collinsboniface1000@gmail.com>
2022-05-03 01:25:57 +0300

    Defined the go program

---
+++ booking-app/main.go
@@ -0,0 +1,11 @@
+package main
+
+// Formart package or fmt
+import "fmt" 
+
+// Where to start the program? Where is the entrypoint
+// The "main" function is the entrypoint of a Go Program
+
+func main() {
+	fmt.Print("Hello World")
+}
