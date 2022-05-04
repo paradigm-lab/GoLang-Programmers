768c260baa29f3e4fda6d495ebea28a87aa11c4a
collins <collinsboniface1000@gmail.com>
2022-05-05 01:58:09 +0300

    Validation to the user input

--- booking-app/main.go
+++ booking-app/main.go
@@ -102,57 +102,81 @@ func main() {
 		fmt.Scan(&userTickets)
 
 
+		// Validation
+		// Logical Operators: &&, ||, !=
+		// First Name and Last Name validation
+		isValidName := len(firstName) >= 2 &&  len(lastName) >= 2
+
+		// Email validation
+		isValidEmail := strings.Contains(email, "@")
+		
+		// Ticket Validation (Needs to enter correct number of ticket positive + greater than 0)
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets 
+
+		// City input
+
 		// Logic to test for the user input if it's greater than then remainingTickets
-		if userTickets > remainingTickets {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
+		if isValidName && isValidEmail && isValidTicketNumber {   
+			// Calculation in go should be done with the same type
+			// To handle this issue is to convert one of them to the other type using the builtIn function that are available in the language
+			remainingTickets = remainingTickets - userTickets
+			// bookings[0] = firstName + " " + lastName
+			bookings = append(bookings, firstName + " " + lastName)	
+
+			/*
+			fmt.Printf("The whole array: %v \n", bookings)
+			fmt.Printf("The first value: %v \n", bookings[0])
+			fmt.Printf("Array type: %T \n", bookings)
+			fmt.Printf("Array length: %v \n", len(bookings))
+			*/
+			
+			/*
+			fmt.Printf("The whole array: %v \n", bookings)
+			fmt.Printf("The first value: %v \n", bookings[0])
+			fmt.Printf("Slice type: %T \n", bookings)
+			fmt.Printf("Slice length: %v \n", len(bookings))
+			*/
+			
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email) 	
+			fmt.Printf("%v ticket remaining for %v \n", remainingTickets, conferenceName)	
+			
+			firstNames := []string{}
+
+			// Range iterates over elements for different data structure (so not only arrys and slices)
+			// Blank Identifier _ 
+			for _, booking := range bookings {
+				// strings.Fields() Splits the strings with white space as separator and returns a slice with the split element
+				var names = strings.Fields(booking)
+				var firstName = names[0]
+				firstNames = append(firstNames, firstName)
+			}
+
+			fmt.Printf("The first names of bookings are: %v \n", firstNames)
+			
+			var noTicketsRemaining bool = remainingTickets == 0 
+
+			if noTicketsRemaining { 
+				// end program
+				fmt.Println("OUr conference is booked out. Come back next yeare.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid")
+			}
+
+
+			// fmt.Printf("Your input data is invalid, try again \n")
+			// fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
 			// continue statement causes loop to skep the remainder of its body and immediately retesting its condition (in our infinite loop our condition is always true)
-			continue
+			// continue
 		}
-
-		// Calculation in go should be done with the same type
-		// To handle this issue is to convert one of them to the other type using the builtIn function that are available in the language
-		remainingTickets = remainingTickets - userTickets
-		// bookings[0] = firstName + " " + lastName
-		bookings = append(bookings, firstName + " " + lastName)	
-
-		/*
-		fmt.Printf("The whole array: %v \n", bookings)
-		fmt.Printf("The first value: %v \n", bookings[0])
-		fmt.Printf("Array type: %T \n", bookings)
-		fmt.Printf("Array length: %v \n", len(bookings))
-		*/
-		
-		/*
-		fmt.Printf("The whole array: %v \n", bookings)
-		fmt.Printf("The first value: %v \n", bookings[0])
-		fmt.Printf("Slice type: %T \n", bookings)
-		fmt.Printf("Slice length: %v \n", len(bookings))
-		*/
-		
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email) 	
-		fmt.Printf("%v ticket remaining for %v \n", remainingTickets, conferenceName)	
-		
-		firstNames := []string{}
-
-		// Range iterates over elements for different data structure (so not only arrys and slices)
-		// Blank Identifier _ 
-		for _, booking := range bookings {
-			// strings.Fields() Splits the strings with white space as separator and returns a slice with the split element
-			var names = strings.Fields(booking)
-			var firstName = names[0]
-			firstNames = append(firstNames, firstName)
-		}
-
-		fmt.Printf("The first names of bookings are: %v \n", firstNames)
-		
-		var noTicketsRemaining bool = remainingTickets == 0 
-
-		if noTicketsRemaining { 
-			// end program
-			fmt.Println("OUr conference is booked out. Come back next yeare.")
-			break
-		}
-
 	}
 
 }
