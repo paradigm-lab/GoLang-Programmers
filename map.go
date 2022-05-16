1b81970b5f3f14419c23bd86d5c2573592e7e119
collins <collinsboniface1000@gmail.com>
2022-05-16 08:27:27 +0300

    Defined a new map in the map file

---
+++ map.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main() {
+
+	statePopulation := map[string] int {
+		"California":	39250017,
+		"Texas":	27962596,
+		"Florida":	20612439,
+		"New York":	19745289,
+		"Pennsylvania": 12802503,
+		"Illinois": 	12891538,
+		"Ohio": 	11614373,
+	}
+	fmt.Println(statePopulation)
+}
